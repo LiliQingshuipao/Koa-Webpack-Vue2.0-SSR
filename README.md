4ac9d5a59cb362ffad0a8cd68002b2965f818a03
holan <358670875@qq.com>
2016-09-06 17:09:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#Koa+Webpack+Vue2.0+SSR
+
+vue2.0 ssr with koa2.0 and webpack
+
+##Installation
+```
+$ npm install
+```
+
+##Building
+```
+$ npm run build
+```
+
+##Start Server
+```
+$ npm run start
+```
+
+##Development
+```
+$ npm run dev
+```
---
+++ src/app/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './demo.vue'
+
+if (typeof window !== 'undefined') {
+
+    new Vue({
+        el: '#app',
+        render: h => h(App)
+    })
+} else {
+
+    module.exports = new Vue({
+        render: h => h(App)
+    })
+}
